ccd47755ab023443e721ff6bb51a992d3816bf54
nick <nickperez1285@gmail.com>
2021-01-09 02:42:17 -0800

    updates

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -22,12 +22,6 @@ class Footer extends Component {
 
 						<ul className="copyright">
 							<li>&copy; Copyright 2020 Nick Perez</li>
-							<li>
-								Design by{" "}
-								<a title="Styleshout" href="http://www.styleshout.com/">
-									Styleshout
-								</a>
-							</li>
 						</ul>
 					</div>
 					<div id="go-top">
